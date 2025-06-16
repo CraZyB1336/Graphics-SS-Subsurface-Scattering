f39d4194a7674136d24544fcf81a6f8d7a401921
CraZyB1336 <chrisbraat5@gmail.com>
2025-06-16 21:41:16 +0200

    feat: find SDF ray data

--- src/radianceCascades/render.cpp
+++ src/radianceCascades/render.cpp
@@ -13,6 +13,7 @@ void renderCascade()
     int dispatchX = (cascadeWidth + WORK_GROUP_SIZE_XY - 1) / WORK_GROUP_SIZE_XY;
     int dispatchY = (cascadeHeight + WORK_GROUP_SIZE_XY - 1) / WORK_GROUP_SIZE_XY;
 
+    glUniform2i(cascadeShader->getUniformFromName("renderResolution"), renderResolution2D.x, renderResolution2D.y);
     glUniform2i(cascadeShader->getUniformFromName("cascadeTextureDimensions"), cascadeWidth, cascadeHeight);
 
     for (unsigned int i = 0; i < cascadeAmount; i++)
@@ -21,9 +22,12 @@ void renderCascade()
 
         int probeSpacing = pow(cascade0Spacing, i + 1);
         int probeRayAmount = probeSpacing * probeSpacing;
+        float probeRayStart = (cascade0Range * (1 - pow(4, i))) / (1 - 4);
+        float probeRayEnd = cascade0Range * pow(4, i);
 
         glUniform1i(cascadeShader->getUniformFromName("probeSpacing"), probeSpacing);
         glUniform1i(cascadeShader->getUniformFromName("probeRayAmount"), probeRayAmount);
+        glUniform2f(cascadeShader->getUniformFromName("probeRayRange"), probeRayStart, probeRayEnd);
         
         glBindImageTexture(0, cascadeTextures[0], 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 
